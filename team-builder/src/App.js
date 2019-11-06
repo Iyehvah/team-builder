e280ec678e08689cdf2262d46e0fac815e2d0036
Iyehvah <Iyehvah@users.noreply.github.com>
2019-11-05 22:06:02 -0600

    Finally done

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,18 +1,19 @@
 import React, {useState} from 'react';
 import './App.css';
 import MemberForm from "./components/MemberForm";
+import Members from "./components/Members";
 
 function App() {
     const [members, setMembers] = useState([{
+        id: 1,
         name: "",
         email: "",
         role: ""
     }
 ]);
-
-
     const addNewMember = member => {
         const newMember = {
+            id: Date.now(),
             name: member.name,
             email: member.email,
             role: member.role
@@ -23,7 +24,9 @@ function App() {
 
     return (
         <div className="App">
+            <h1>Add A Team Member!</h1>
             <MemberForm addNewMember={addNewMember}/>
+            <Members members={members}/>
         </div>
     )
 }
--- team-builder/src/components/MemberForm.js
+++ team-builder/src/components/MemberForm.js
@@ -10,13 +10,51 @@ const MemberForm = props => {
     const handleChanges = event => {
         setMembers({...members, [event.target.name]: event.target.value });
     };
+    const submitHandler = event => {
+        event.preventDefault();
+        const newMember = {
+            ...members,
+            id: Date.now()
+        }
+        props.addNewMember(newMember);
+        setMembers({ name: "", email: "", role: "" });
+    };
 
     return(
-        <div>
-            <h1>{members.name}</h1>
-            <form>
-                <label htmlFor="title">Title Here</label>
-                <textArea></textArea>
+        <div className="AddingMember">
+            
+            <form onSubmit={submitHandler}>
+               
+                <label htmlFor="name">Member Name</label>
+                <input 
+                onChange={handleChanges}
+                id="name"
+                type="text"
+                name="name"
+                value={members.name}
+                />
+
+               
+                <label htmlFor="email">Member Email</label>
+                <input
+                onChange={handleChanges}
+                id="email"
+                type="text"
+                name="email"
+                value={members.email} 
+                />
+
+                
+                <label htmlFor="role">Members Role</label>
+                <input 
+                onChange={handleChanges}
+                id="role"
+                type="text"
+                name="role"
+                value={members.role} 
+                />
+
+                <button type="submit">Submit</button>
             </form>
         </div>
     )
